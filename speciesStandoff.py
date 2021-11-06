74709f908ba00bc064b1408ed6d37727f0b37e8b
jamtip <jamesc.tipton@gmail.com>
2021-11-05 23:21:13 -0500

    added to animal.py and made sprites

--- speciesStandoff.py
+++ speciesStandoff.py
@@ -1,6 +1,8 @@
 from importlib.abc import Loader
 import kivy
 import os
+
+from kivy.logger import BLACK
 kivy.require('2.0.0') # replace with your current kivy version !
 
 from kivy import *
@@ -13,11 +15,14 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 
+Builder.load_file("speciesStandoff.kv")
+
 class FightScreen(GridLayout):
     def __init__(self, **kwargs):
         super(FightScreen, self).__init__(**kwargs)
         self.cols = 2
         self.rows = 2
+        self.spacing = 30
         self.add_widget(Label(text='Enemies'))
         self.add_widget(Label(text='Friends'))
         self.add_widget(Container())
@@ -27,13 +32,18 @@ class Container(GridLayout):
     def __init__(self, **kwargs):
         super(Container, self).__init__(**kwargs)
         self.cols = 5
-        with self.canvas:
-            Color(1.0, 0.5, 0.5, 1.0)
+        self.spacing = 10
+        self.add_widget(Label(text='1'))
+        self.add_widget(Label(text='2'))
+        self.add_widget(Label(text='3'))
+        self.add_widget(Label(text='4'))
+        self.add_widget(Label(text='5'))
+
+        
 
 class speciesStandoff(App):
 
     def build(self):
-        Builder.load_file("speciesStandoff.kv")
         return FightScreen()
 
 

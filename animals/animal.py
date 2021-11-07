57eab794f0e8e840dcd284cc39bb4e71769c31aa
jamtip <jamesc.tipton@gmail.com>
2021-11-06 23:34:40 -0500

    removed old animal and replaced it with working version

--- animals/animal.py
+++ animals/animal.py
@@ -9,29 +9,24 @@ class Animal:
         self.toughness = args[1]
         self.species = ""
     
-    dodge_chance = 0
-    crit_chance = 0
+        self.dodge_chance = 0
+        self.crit_chance = 0
 
-    neg_effects = { "burned":0,
-                    "poisoned":0,
+        self.neg_effects = { "burned":0,
+                        "poisoned":0,
+                        }
+        
+        self.mutator = { "fire":0,
+                    "poison":0,
+                    "shock":0,
+                    "armor":0,
                     }
-    
-    mutator = { "fire":0,
-                "poison":0,
-                "shock":0,
-                "armor":0,
-                }
 
-    on_death = {"milk":0,
-                "egg":0,
-                }
+        self.on_death = {"milk":0,
+                    "egg":0,
+                    }
 
-    # death_q[0] = friends
-    # death_q[1] = enemies
-    death_q = [[],[]]
 
-    def die(self, list1, list2, index, dq_index):
-        Animal.death_q[dq_index].append(self)
 
 
 class Cow(Animal):
@@ -44,12 +39,26 @@ class Cow(Animal):
         self.image = "./images/cow.png"
 
         #special attributes
-        self.on_death["milk"] = 2
-    
-    def die(self, list1, list2, index, dq_index):
-        Animal.death_q[dq_index].append(self)
+        self.dodge_chance = 0
+        self.crit_chance = 0
+
+        self.neg_effects = { "burned":0,
+                        "poisoned":0,
+                        }
+        
+        self.mutator = { "fire":0,
+                    "poison":0,
+                    "shock":0,
+                    "armor":0,
+                    }
+
+        self.on_death = {"milk":2,
+                    "egg":0,
+                    }
+
+    def die(self, friends, foes, index):
         if index == -1: return
-        list1[index].toughness += self.on_death["milk"]
+        friends[index].toughness += self.on_death["milk"]
 
 
 class Chicken(Animal):
@@ -62,13 +71,27 @@ class Chicken(Animal):
         self.image = "./images/chicken.png"
 
         #special attributes
-        self.on_death["egg"] = 1
+        self.dodge_chance = 0
+        self.crit_chance = 0
 
-    def die(self, list1, list2, index, dq_index):
-        Animal.death_q[dq_index].append(self)
-        if len(list2) == 0: return
-        (random.choice(list2)).toughness -= self.on_death["egg"]
-        print("egg thrown")
+        self.neg_effects = { "burned":0,
+                            "poisoned":0,
+                            }
+        
+        self.mutator = { "fire":0,
+                        "poison":0,
+                        "shock":0,
+                        "armor":0,
+                        }
+
+        self.on_death = {"milk":0,
+                        "egg":1,
+                        }
+
+    def die(self, friends, foes, index):
+        if len(foes) == 0: return
+        (random.choice(foes)).toughness -= self.on_death["egg"]
+    #     print("egg thrown")
 
 
 class Snake(Animal):
@@ -81,7 +104,25 @@ class Snake(Animal):
         self.image = "./images/snake.png"
 
         #special attributes
-        self.mutator["poison"] = 1
+        self.dodge_chance = 0
+        self.crit_chance = 0
+
+        self.neg_effects = { "burned":0,
+                            "poisoned":0,
+                            }
+        
+        self.mutator = { "fire":0,
+                        "poison":1,
+                        "shock":0,
+                        "armor":0,
+                        }
+
+        self.on_death = {"milk":0,
+                        "egg":0,
+                        }
+        
+    def die(self, friends, foes, index):
+        pass
 
 
 
@@ -95,7 +136,25 @@ class Fish(Animal):
         self.image = "./images/fish.png"
 
         #special attributes
+        self.dodge_chance = 0
         self.crit_chance = .25
+
+        self.neg_effects = { "burned":0,
+                            "poisoned":0,
+                            }
+        
+        self.mutator = { "fire":0,
+                        "poison":0,
+                        "shock":0,
+                        "armor":0,
+                        }
+
+        self.on_death = {"milk":0,
+                        "egg":0,
+                        }
+        
+    def die(self, friends, foes, index):
+        pass
 
 
 
@@ -109,7 +168,25 @@ class Eel(Animal):
         self.image = "./images/eel.png"
 
         #special attributes
-        self.mutator["shock"] = 1
+        self.dodge_chance = 0
+        self.crit_chance = 0
+
+        self.neg_effects = { "burned":0,
+                            "poisoned":0,
+                            }
+        
+        self.mutator = { "fire":0,
+                        "poison":0,
+                        "shock":1,
+                        "armor":0,
+                        }
+
+        self.on_death = {"milk":0,
+                        "egg":0,
+                        }
+        
+    def die(self, friends, foes, index):
+        pass
 
 
 
@@ -123,6 +200,25 @@ class Dog(Animal):
         self.image = "./images/dog.png"
 
         #special attributes
+        self.dodge_chance = 0
+        self.crit_chance = 0
+
+        self.neg_effects = { "burned":0,
+                            "poisoned":0,
+                            }
+        
+        self.mutator = { "fire":0,
+                        "poison":0,
+                        "shock":0,
+                        "armor":0,
+                        }
+
+        self.on_death = {"milk":0,
+                        "egg":0,
+                        }
+        
+    def die(self, friends, foes, index):
+        pass
 
 
 
@@ -136,6 +232,25 @@ class Cat(Animal):
         self.image = "./images/cat.png"
 
         #special attributes
+        self.dodge_chance = 0
+        self.crit_chance = 0
+
+        self.neg_effects = { "burned":0,
+                            "poisoned":0,
+                            }
+        
+        self.mutator = { "fire":0,
+                        "poison":0,
+                        "shock":0,
+                        "armor":0,
+                        }
+
+        self.on_death = {"milk":0,
+                        "egg":0,
+                        }
+        
+    def die(self, friends, foes, index):
+        pass
 
 
 
@@ -150,6 +265,24 @@ class Crow(Animal):
 
         #special attributes
         self.dodge_chance = .2
+        self.crit_chance = 0
+
+        self.neg_effects = { "burned":0,
+                            "poisoned":0,
+                            }
+        
+        self.mutator = { "fire":0,
+                        "poison":0,
+                        "shock":0,
+                        "armor":0,
+                        }
+
+        self.on_death = {"milk":0,
+                        "egg":0,
+                        }
+        
+    def die(self, friends, foes, index):
+        pass
 
 
 class Penguin(Animal):
@@ -162,6 +295,25 @@ class Penguin(Animal):
         self.image = "./images/penguin.png"
 
         #special attributes
+        self.dodge_chance = 0
+        self.crit_chance = 0
+
+        self.neg_effects = { "burned":0,
+                            "poisoned":0,
+                            }
+        
+        self.mutator = { "fire":0,
+                        "poison":0,
+                        "shock":0,
+                        "armor":0,
+                        }
+
+        self.on_death = {"milk":0,
+                        "egg":0,
+                        }
+        
+    def die(self, friends, foes, index):
+        pass
 
 
 
@@ -175,6 +327,25 @@ class PolarBear(Animal):
         self.image = "./images/polarbear.png"
 
         #special attributes
+        self.dodge_chance = 0
+        self.crit_chance = 0
+
+        self.neg_effects = { "burned":0,
+                            "poisoned":0,
+                            }
+        
+        self.mutator = { "fire":0,
+                        "poison":0,
+                        "shock":0,
+                        "armor":0,
+                        }
+
+        self.on_death = {"milk":0,
+                        "egg":0,
+                        }
+        
+    def die(self, friends, foes, index):
+        pass
 
 
 
@@ -188,6 +359,25 @@ class Panda(Animal):
         self.image = "./images/panda.png"
 
         #special attributes
+        self.dodge_chance = 0
+        self.crit_chance = 0
+
+        self.neg_effects = { "burned":0,
+                            "poisoned":0,
+                            }
+        
+        self.mutator = { "fire":0,
+                        "poison":0,
+                        "shock":0,
+                        "armor":0,
+                        }
+
+        self.on_death = {"milk":0,
+                        "egg":0,
+                        }
+        
+    def die(self, friends, foes, index):
+        pass
 
 
 
@@ -201,8 +391,25 @@ class Glipglop(Animal):
         self.image = "./images/glipglop.png"
 
         #special attributes
-        self.mutator["poison"] = 1
+        self.dodge_chance = 0
         self.crit_chance = .5
+
+        self.neg_effects = { "burned":0,
+                            "poisoned":0,
+                            }
+        
+        self.mutator = { "fire":0,
+                        "poison":1,
+                        "shock":0,
+                        "armor":0,
+                        }
+
+        self.on_death = {"milk":0,
+                        "egg":0,
+                        }
+        
+    def die(self, friends, foes, index):
+        pass
 
 
 
@@ -216,7 +423,25 @@ class Sweeble(Animal):
         self.image = "./images/sweeble.png"
 
         #special attributes
-        self.mutator["poison"] = 1
+        self.dodge_chance = 0
+        self.crit_chance = 0
+
+        self.neg_effects = { "burned":0,
+                            "poisoned":0,
+                            }
+        
+        self.mutator = { "fire":0,
+                        "poison":1,
+                        "shock":0,
+                        "armor":0,
+                        }
+
+        self.on_death = {"milk":0,
+                        "egg":0,
+                        }
+        
+    def die(self, friends, foes, index):
+        pass
 
 
 
@@ -230,7 +455,25 @@ class Gnekk(Animal):
         self.image = "./images/gnekk.png"
 
         #special attributes
+        self.dodge_chance = 0
         self.crit_chance = .33
+
+        self.neg_effects = { "burned":0,
+                            "poisoned":0,
+                            }
+        
+        self.mutator = { "fire":0,
+                        "poison":0,
+                        "shock":0,
+                        "armor":0,
+                        }
+
+        self.on_death = {"milk":0,
+                        "egg":0,
+                        }
+        
+    def die(self, friends, foes, index):
+        pass
 
 
 
@@ -244,48 +487,80 @@ class Loodle(Animal):
         self.image = "./images/loodle.png"
 
         #special attributes
-        self.mutator["fire"] = 2
         self.dodge_chance = .25
+        self.crit_chance = 0
+
+        self.neg_effects = { "burned":0,
+                            "poisoned":0,
+                            }
+        
+        self.mutator = { "fire":2,
+                        "poison":0,
+                        "shock":0,
+                        "armor":0,
+                        }
+
+        self.on_death = {"milk":0,
+                        "egg":0,
+                        }
+        
+    def die(self, friends, foes, index):
+        pass
+
+class Niugnep(Animal):
+    def __init__(self, *args, **kwargs):
+        self.species = "inverse penguin"
+        self.desc = "not so useless now. also burns."
+        self.power, self.toughness = 3, 2
+        if args:
+            self.power, self.toughness = args[0], args[1]
+        self.image = "./images/loodle.png"
+
+        #special attributes
+        self.dodge_chance = 0
+        self.crit_chance = 0
+
+        self.neg_effects = { "burned":0,
+                            "poisoned":0,
+                            }
+        
+        self.mutator = { "fire":1,
+                        "poison":0,
+                        "shock":0,
+                        "armor":0,
+                        }
+
+        self.on_death = {"milk":0,
+                        "egg":0,
+                        }
+        
+    def die(self, friends, foes, index):
+        pass
 
 
+def fight(friends, foes):
 
-
-def fight(list1, list2):
-
-    list2.reverse()
-    while len(list1) != 0 and len(list2) != 0:
-
-        for animal in list1:
-            print(animal.power, animal.toughness)
-        print("---")
-        for animal in list2:
-            print(animal.power, animal.toughness)
-        print("--------")
+    foes.reverse()
+    while len(friends) != 0 and len(foes) != 0:
 
         #before attack - takes a list and returns it after all status damage is done (burn, poison etc)
-        list1 = pre_attack_damage(list1, list2, 0)
-        list2 = pre_attack_damage(list2, list1, 1)
+        friends = pre_attack_damage(friends, foes)
+        foes = pre_attack_damage(foes, friends)
 
 
         #attack - takes lists 1 and 2 and returns the lists after animals fight
-        list1, list2 = attack(list1, list2)
+        friends, foes = attack(friends, foes)
 
         #after attack - loops through dead animals and applies "on_death" attributes
 
         #end phase - loops through death queues and handles the results of who died this round
-        for animal in Animal.death_q[0]:
-            print(animal.species, " died!")
-        Animal.death_q[0].clear()
-        for animal in Animal.death_q[1]:
-            print(animal.species, " died!")
-        Animal.death_q[1].clear()
 
 
     # return the lists with possible victors, or empty lists for a tie
-    return list1, list2
+    return friends, foes
 
 
-def pre_attack_damage(team, enemy, death_q_index):
+def pre_attack_damage(team, enemy):
     animal: Animal
     for animal in team:
 
@@ -296,84 +571,71 @@ def pre_attack_damage(team, enemy, death_q_index):
         animal.toughness -= animal.neg_effects["poisoned"]
 
         if animal.toughness <= 0:
-            animal.die(team, enemy, team.index(animal), death_q_index)
+            index = team.index(animal)
+            if index == len(team)-1: index -= 1
             team.remove(animal)
+            animal.die(team, enemy, index)
 
     return team
 
-def attack(list1, list2):
+def attack(friends, foes):
     
-    if len(list1) > 0:
-        animal1 = list1[0]
-    else: return list1, list2
+    if len(friends) > 0:
+        friend = friends[0]
+    else: return friends, foes
 
-    if len(list2) > 0:
-        animal2 = list2[0]
-    else: return list1, list2
+    if len(foes) > 0:
+        foe = foes[0]
+    else: return friends, foes
 
-    print(f"{animal1.species} - {animal1.power}/{animal1.toughness} vs {animal2.species} - {animal2.power}/{animal2.toughness}")
+    print(f"{friend.species} - {friend.power}/{friend.toughness} vs {foe.species} - {foe.power}/{foe.toughness}")
         
-    # animal 1 combat
-    if random.random() > animal1.dodge_chance:
-        # if animal 1 doesnt dodge
-        if random.random() <= animal2.crit_chance:
-            # if animal 2 crits
-            print(animal2.species, " crit!")
-            animal1.toughness = animal1.toughness - math.ceil(animal2.power * 1.5)
+    # combat
+    combat(friend, foe, friends)
+    combat(foe, friend, foes)
 
-        # else normal combat
-        else: animal1.toughness = animal1.toughness - animal2.power
-    else:
-        print(animal1.species, " dodged!")
-    
-    # add special negative effects after combat and dodge
-    animal1.neg_effects["burned"] = animal2.mutator["fire"]
-    animal1.neg_effects["poisioned"] = animal2.mutator["poison"]
-    if list1.index(animal1) != len(list1)-1:
-        list1[list1.index(animal1)+1].toughness - animal2.mutator["shock"]
-
-    # animal 2 combat
-    if random.random() > animal2.dodge_chance:
-        # if animal 2 doesnt dodge
-        if random.random() <= animal1.crit_chance:
-            # if animal 2 crits
-            print(animal1.species, " crit!")
-            animal2.toughness = animal2.toughness - math.floor(animal1.power * 1.5)
-        
-        # else normal combat
-        else: animal2.toughness = animal2.toughness - animal1.power
-    else:
-        print(animal2.species," dodged!")
-    
-    # add special negative effects after combat and dodge
-    animal2.neg_effects["burned"] = animal1.mutator["fire"]
-    animal2.neg_effects["poisioned"] = animal1.mutator["poison"]
-    if list2.index(animal2) != len(list2)-1:
-        list2[list2.index(animal2)+1].toughness - animal1.mutator["shock"]
-    
     # on die
-    if animal1.toughness <= 0:
-        index = list1.index(animal1)
-        # if dead animal is last in the list
-        if index == len(list1)-1: index -= 1
-        animal1.die(list1, list2, index, 0)
-        list1.remove(animal1)
+    if friend.toughness <= 0:
+        index = friends.index(friend)
+        if index == len(friends)-1: index -= 1
+        friends.remove(friend)
+        friend.die(friends, foes, index)
 
-    if animal2.toughness <= 0:
-        index = list2.index(animal2)
+    if foe.toughness <= 0:
+        index = foes.index(foe)
         # if dead animal is last in the list
-        if index == len(list2)-1: index -= 1
-        animal2.die(list2, list1, index, 1)
-        list2.remove(animal2)
+        if index == len(foes)-1: index -= 1
+        foes.remove(foe)
+        foe.die(friends, foes, index)
 
-    return list1, list2
+    return friends, foes
+
+def combat(team, enemy, teammates):
+    if random.random() > team.dodge_chance:
+        # if teammate doesnt dodge
+        if random.random() <= enemy.crit_chance:
+            # if enemy crits
+            print(enemy.species, " crit!")
+            team.toughness = team.toughness - math.ceil(enemy.power * 1.5)
+
+        # else normal combat
+        else: team.toughness = team.toughness - enemy.power
+    else:
+        print(team.species, " dodged!")
+    
+    # add special negative effects after combat and dodge
+    team.neg_effects["burned"] = enemy.mutator["fire"]
+    team.neg_effects["poisoned"] = enemy.mutator["poison"]
+
+    if teammates.index(team) != len(teammates)-1:
+        teammates[teammates.index(team)+1].toughness - enemy.mutator["shock"]
 
 ### TESTING TESTING ONE TWO THREE TESTING TESTING ###
-list1 = [PolarBear(), Loodle(), Gnekk(), Snake(), Dog()]
-list2 = [Chicken(), Chicken(), Panda(), Glipglop(), Cow()]
+friends = [PolarBear(), Loodle(), Gnekk(), Snake(), Dog()]
+foes = [Chicken(), Chicken(), Panda(), Glipglop(), Cow()]
 
 
-res1, res2 = fight(list1, list2)
+res1, res2 = fight(friends, foes)
 
 if len(res1) == 0 and len(res2) == 0:
     print("tie!")
